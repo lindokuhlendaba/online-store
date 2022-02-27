4bfbec57b8642074fd6cbb7ab3ada727e338db04
Lindokuhle Ndaba <Lindokuhle.ndaba@chillisoft.co.za>
2022-02-27 23:03:57 +0200

    wip" Products styling

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,13 +6,13 @@ export default class Products extends Component<any>{
         console.log("this.props.products")
         console.log(this.props.products)
         const productItems = this.props.products.map(product => (
-            <div className="col-md-4" key={product.id}>
+            <div className="col-xs-6 col-md-3" key={product.id}>
                 <div className="thumbnail text-center">
                     <a href={`#${product.id}`} onClick={this.props.handleAddToCart}>
                         <img src={product.image} alt={product.title} />
-                        <p>
+                        <div className="caption">
                             {product.title}
-                        </p>
+                        </div>
                     </a>
                     <div>
                         <b>{formatCurrency(product.price)}</b>
