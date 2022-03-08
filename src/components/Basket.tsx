fa03736c857560317497b3c612a0856d4402a638
Lindokuhle Ndaba <Lindokuhle.ndaba@chillisoft.co.za>
2022-03-08 22:57:29 +0200

    wip: show product details on cart component

--- src/components/Basket.tsx
+++ src/components/Basket.tsx
@@ -6,6 +6,25 @@ class Basket extends Component<any> {
         return (
             <div className="alert alert-info">
                 {cartItems.length === 0 ? "Cart is empty" : <div> You have {cartItems.length} in your cart </div>}
+                {cartItems.length > 0 &&
+
+                    <div>
+                        <ul>
+                            {cartItems.map(item =>
+                                <li>
+                                    <b>{item.title}</b>
+                                     X {item.count}
+                                    <button className="btn btn-danger"
+                                        onClick={(e) => this.props.handleRemoveFromCart(e, item)}
+                                    >X</button>
+                                </li>
+
+                            )}
+                        </ul>
+
+
+                    </div>
+                }
             </div>
         );
     }
